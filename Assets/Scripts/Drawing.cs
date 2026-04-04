87ff96b71e1a7a887e38abe4bd4a967aace852b1
JokinhasProgramassoes <joaotomaspereiraribeiro2004@gmail.com>
2026-04-05 00:44:20 +0100

    pimba
    tela feita - limite de strokes
    tenho que por mais pinceis, mas ns como....... pensei a crio um novo pincel mas dps o problema é que so teria uma cor pq e uma textura.... pragmatico

--- Assets/Scripts/Drawing.cs
+++ Assets/Scripts/Drawing.cs
@@ -1,25 +1,54 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using JetBrains.Annotations;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.Rendering;
 
 public class Drawing : MonoBehaviour
 {
     public Camera m_camera;
+    #region Cores dos Strokes
     public GameObject brushRed;
     public GameObject brushBlue;
     public GameObject brushGreen;
     public GameObject brushWhite;
-
+    #endregion
     public int brushCount;
 
     LineRenderer currentLineRenderer;
-
     Vector2 lastPos;
 
+    //Limite do canvas--------------------------
+    public float canvaXMin = 0.0f, canvaXMax = 1.0f;
+    public float canvaYMin = 0.0f, canvaYMax = 1.0f;
+    
     void Update()
     {
+        
         Draw();
+    }
+
+    Vector2 GetRawMousePos()
+    {
+        return m_camera.ScreenToWorldPoint(Input.mousePosition);
+    }
+
+    Vector2 GetClampedMousePos()
+    {
+        Vector2 mousePos = m_camera.ScreenToWorldPoint(Input.mousePosition);
+        //Clamp do tamanho do canvas:
+        mousePos.x = Mathf.Clamp(mousePos.x, canvaXMin, canvaXMax);
+        mousePos.y = Mathf.Clamp(mousePos.y, canvaYMin, canvaYMax); 
+
+        return mousePos;
+    }
+
+    bool IsInsideCanvas (Vector2 pos)
+    {
+        return pos.x >= canvaXMin && pos.x <= canvaXMax &&
+        pos.y >= canvaYMin && pos.y <= canvaYMax; 
     }
 
     void AddPoint(Vector2 pointPos)
@@ -29,9 +58,10 @@ public class Drawing : MonoBehaviour
         currentLineRenderer.SetPosition(positionIndex, pointPos);
     }
 
-    void PointToMousePos()
+     void PointToMousePos()
     {
-        Vector2 mousePos = m_camera.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 mousePos = GetClampedMousePos();
+        
         if(lastPos != mousePos)
         {
             AddPoint(mousePos);
@@ -42,31 +72,30 @@ public class Drawing : MonoBehaviour
 
     void Draw()
     {
+        Vector2 rawMousePos = GetRawMousePos();
+
         if (Input.GetKeyDown(KeyCode.Mouse0))
         {
-            if(brushCount == 0)
-            {
-                //CreateBrushRed();
-            }
-
-             if(brushCount == 1)
-            {
-                //CreateBrushGreen();
-            }
-
-             if(brushCount == 2)
-            {
-                //CreateBrushBlue();
-            }
-
-             if(brushCount == 3)
-            {
-                //CreateBrushWhite();
-            }
+            
+            if (!IsInsideCanvas(rawMousePos)) return;
+            
+            if(brushCount == 0) CreateBrushRed();
+            if(brushCount == 1) CreateBrushGreen();
+            if(brushCount == 2) CreateBrushBlue();
+            if(brushCount == 3) CreateBrushWhite();
+        
         }
 
-        else if (Input.GetKeyDown(KeyCode.Mouse0))
+        else if (Input.GetKey(KeyCode.Mouse0))
         {
+            if(currentLineRenderer == null) return;
+
+            if (!IsInsideCanvas(rawMousePos))
+            {
+                currentLineRenderer = null;
+                return;
+            }
+
             PointToMousePos();
         }
         else
@@ -75,7 +104,7 @@ public class Drawing : MonoBehaviour
         }
     }
 
-    //Buttons - defernetes cores:
+    #region EscolhaCor
     public void RedButton()
     {
         brushCount = 0;
@@ -92,44 +121,63 @@ public class Drawing : MonoBehaviour
     {
         brushCount = 3;
     } 
+    #endregion
 
+    #region Brushes
     void CreateBrushRed()
     {
         GameObject brushInstance = Instantiate (brushRed);
         currentLineRenderer = brushInstance.GetComponent<LineRenderer>();
-        // 2 pontos fazem uma linha:
-        Vector2 mousePos = m_camera.ScreenToWorldPoint(Input.mousePosition);
+        
+        Vector2 mousePos = GetClampedMousePos();
+
+        currentLineRenderer.positionCount = 2;
         currentLineRenderer.SetPosition(0, mousePos);
         currentLineRenderer.SetPosition(1, mousePos);
+
+        lastPos = mousePos;
     }
 
     void CreateBrushGreen()
     {
         GameObject brushInstance = Instantiate (brushGreen);
         currentLineRenderer = brushInstance.GetComponent<LineRenderer>();
-        // 2 pontos fazem uma linha:
-        Vector2 mousePos = m_camera.ScreenToWorldPoint(Input.mousePosition);
+        
+        Vector2 mousePos = GetClampedMousePos();
+
+        currentLineRenderer.positionCount = 2;
         currentLineRenderer.SetPosition(0, mousePos);
         currentLineRenderer.SetPosition(1, mousePos);
+
+        lastPos = mousePos;
     }
 
     void CreateBrushBlue()
     {
         GameObject brushInstance = Instantiate (brushBlue);
         currentLineRenderer = brushInstance.GetComponent<LineRenderer>();
-        // 2 pontos fazem uma linha:
-        Vector2 mousePos = m_camera.ScreenToWorldPoint(Input.mousePosition);
+        
+        Vector2 mousePos = GetClampedMousePos();
+
+        currentLineRenderer.positionCount = 2;
         currentLineRenderer.SetPosition(0, mousePos);
         currentLineRenderer.SetPosition(1, mousePos);
+
+        lastPos = mousePos;
     }
 
     void CreateBrushWhite()
     {
         GameObject brushInstance = Instantiate (brushWhite);
         currentLineRenderer = brushInstance.GetComponent<LineRenderer>();
-        // 2 pontos fazem uma linha:
-        Vector2 mousePos = m_camera.ScreenToWorldPoint(Input.mousePosition);
+        
+        Vector2 mousePos = GetClampedMousePos();
+
+        currentLineRenderer.positionCount = 2;
         currentLineRenderer.SetPosition(0, mousePos);
         currentLineRenderer.SetPosition(1, mousePos);
+
+        lastPos = mousePos;
     }
+    #endregion
 }
